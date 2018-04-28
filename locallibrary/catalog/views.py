a079cddad3535264a0988addf20170668502e296
Brennen Berkley <hello@brennenberkley.com>
2018-04-28 09:59:27 -0600

    Added pages for browsing the library's catalog
    Added list and detail pages for books and authors

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Book, Author, BookInstance, Genre
+from django.views import generic
 
 def index(request):
     # Generate counts of some of the main objects
@@ -11,12 +12,25 @@ def index(request):
 
     # Render the HTML template index.html with the data in the context variable
     return render(
-        request,
-        'index.html',
-        context={
-        'num_books':num_books,
-        'num_instances':num_instances,
-        'num_instances_available':num_instances_available,
-        'num_authors':num_authors
-        },
-    )
+    request,
+    'index.html',
+    context={
+    'num_books':num_books,
+    'num_instances':num_instances,
+    'num_instances_available':num_instances_available,
+    'num_authors':num_authors
+    },
+)
+
+class BookListView(generic.ListView):
+    model = Book
+    paginate_by = 5
+
+class BookDetailView(generic.DetailView):
+    model = Book
+
+class AuthorListView(generic.ListView):
+    model = Author
+
+class AuthorDetailView(generic.DetailView):
+    model = Author
