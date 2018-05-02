b3bbcf8dcf937eb208ff48fdaa15fa003e8431f1
Brennen Berkley <hello@brennenberkley.com>
2018-05-02 11:03:48 -0600

    Added the 'borrower' field to BookInstance so that users can check out books

--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -1,6 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 from django.urls import reverse #Used to generate URLs by reversing the URL patterns
 import uuid # Required for unique book instances
+from datetime import date
 
 class Genre(models.Model):
     name = models.CharField(
@@ -44,6 +46,11 @@ class BookInstance(models.Model):
     book = models.ForeignKey('Book', on_delete=models.SET_NULL, null=True)
     imprint = models.CharField(max_length=200)
     due_back = models.DateField(null=True, blank=True)
+    borrower = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+
+    @property
+    def is_overdue(self):
+        return self.due_back and date.today() > self.due_back
 
     LOAN_STATUS = (
         ('m', 'Maintenance'),
