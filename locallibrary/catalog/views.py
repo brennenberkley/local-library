ee5067220d4cffcffd65c83beb507e5c7bcc2326
Brennen Berkley <hello@brennenberkley.com>
2018-05-02 11:22:37 -0600

    Added a page where users can view their checked out books

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Book, Author, BookInstance, Genre
+from django.contrib.auth.models import User
 from django.views import generic
 
 def index(request):
@@ -39,3 +40,13 @@ class AuthorListView(generic.ListView):
 
 class AuthorDetailView(generic.DetailView):
     model = Author
+
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+class LoanedBooksByUserListView(LoginRequiredMixin,generic.ListView):
+    model = BookInstance
+    template_name ='catalog/bookinstance_list_borrowed_user.html'
+
+    def get_queryset(self):
+        return BookInstance.objects.filter(borrower=self.request.user) \
+        .filter(status__exact='o').order_by('due_back')
